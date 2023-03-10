c75447aa73e4603aca58077e18f89f4439f95ddf
HemangVora <hemangvora2@gmail.com>
2023-03-10 15:04:03 +0530

    buy desktop changes

--- src/pages/Buy.js
+++ src/pages/Buy.js
@@ -78,8 +78,74 @@ function Buy() {
   };
   return (
     <>
-      {" "}
-      {listingData && (
+{ listingData && (
+  
+        <motion.div
+          variants={staggerContainer}
+          initial="initial"
+          animate="animate"
+          className="items-center flex justify-between flex-col"
+        >
+         
+          {/* <motion.div variants={fadeInUp} className="flex justify-center mt-5">
+          <img
+            className="h-44 w-44 hover:scale-105 hover:shadow-lg"
+            src={nft}
+            alt="nft"
+          />
+        </motion.div> */}
+          <motion.div
+            variants={fadeInUp}
+            className=" flex justify-center  p-5 overflow-hidden	"
+          >
+            <img
+              className="object-none h-[40vh] w-full  "
+              src={listingData.image}
+              alt="nft"
+            />
+          </motion.div>
+          <motion.div variants={fadeInUp} className="flex justify-between w-40 mt-5">
+            <p className="font-opensans text-xl font-bold text-darkbrown">
+              Piece #{listingData.id}
+            </p>
+            <p className="font-opensans text-lg text-darkbrown">
+              ${listingData.amount}
+            </p>
+          </motion.div>
+          <motion.div variants={fadeInUp} className="flex justify-center mt-7">
+            <button className=" hover:scale-105 transition-all ease-out font-opensans w-96 rounded-md px-4 py-4 bg-opacity-90 text-stone-200 bg-black ">
+              <img src={apple2} className="h-6 w-5 -mb-5 ml-32" />
+              <p className="text-white font-opensans">Pay</p>
+            </button>
+          </motion.div>
+
+          <motion.div
+            variants={fadeInUp}
+            className="mt-10 ml-3 mr-3 border-t-2 border-ochredark"
+          >
+            <div className="h-8 w-fit font-normal mx-auto px-3 text-ochredark bg-ochre -mt-4 ">
+              <p className="font-opensans">Or Pay with card</p>
+            </div>
+          </motion.div>
+
+          <motion.div variants={fadeInUp}>
+            <p className="lg:text-center font-bold lg:pl-88 lg:pr-80 mt-6 ml-5 text-lg font-opensans text-darkbrown">
+              Card info
+            </p>
+            <div>
+              <form method="GET" id="my_form"></form>
+              <Elements stripe={stripePromise}>
+                <PaymentForm
+                  amount={listingData.amount}
+                  listingId={listingData.id}
+                  twitterId={userData.twitterId}
+                />
+              </Elements>
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+      {isTabletOrMobile && listingData && (
         <motion.div
           variants={staggerContainer}
           initial="initial"
