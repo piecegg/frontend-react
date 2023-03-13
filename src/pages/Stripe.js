e49d4d847e1f3b3511e14eadbe9a5d9b5e71f38e
Sakshi Shah <sakshi.shah299@gmail.com>
2023-03-13 23:00:44 +0530

    public key needed for stripe

--- src/pages/Stripe.js
+++ src/pages/Stripe.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+const stripePromise = loadStripe("pk_test_51Mg5OdSF3WP5DzSyRT0uf0LrF4AD0qfykJqnUdp0hTlGJOLLiOiEhNQ7zTmRP2xhFrbvix10LoZ66THIOJVU9ERI00Yz6Luprs");
+
 function Stripe() {
   return (
     <Elements stripe={stripePromise}>
